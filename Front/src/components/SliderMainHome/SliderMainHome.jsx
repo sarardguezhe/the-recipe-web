e1b8e8c1030559f42fe26f2993ee24974e7756db
sarardguezhe <sara.rodriguezhe@gmail.com>
2026-04-30 21:47:35 +0200

    fix: webkit-clip-path

--- Front/src/components/SliderMainHome/SliderMainHome.jsx
+++ Front/src/components/SliderMainHome/SliderMainHome.jsx
@@ -13,62 +13,54 @@ import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
 
 
 export default function App() {
-  const videoStyle = {
-    width: "100%",
-    maxHeight: "600px",
-    objectFit: "cover",
-  };
-
-  const swiperContainerStyle = {
-    width: "100%",
-    margin: "0 auto",
-    clipPath: "ellipse(120% 60% at 50% 40%)",
-    WebkitClipPath: "ellipse(120% 60% at 50% 40%)",
-    overflow: "hidden",
-  };
 
   return (
-    <div style={swiperContainerStyle}>
-      <Swiper
-        centeredSlides={true}
-        effect={'fade'}
-        autoplay={{
-          delay: 7000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: false,
-        }}
-        navigation
-        modules={[EffectFade, Navigation, Pagination, Autoplay]}
-      >
-        <SwiperSlide>
-          <video autoPlay muted loop style={videoStyle}>
-            <source
-              src="https://res.cloudinary.com/dc3pogjef/video/upload/v1693791075/Anuncio_para_Redes_Asia%CC%81tico_Restaurante_Entrega_a_Domicilio_8_igxllh.mp4"
-              type="video/mp4"
-            />
-          </video>
-        </SwiperSlide>
-        <SwiperSlide>
-          <video autoPlay muted loop style={videoStyle}>
-            <source
-              src="https://res.cloudinary.com/dc3pogjef/video/upload/v1693791075/Anuncio_para_Redes_Asia%CC%81tico_Restaurante_Entrega_a_Domicilio_7_clwjri.mp4"
-              type="video/mp4"
-            />
-          </video>
-        </SwiperSlide>
-        <SwiperSlide>
-          <video autoPlay muted loop style={videoStyle}>
-            <source
-              src="https://res.cloudinary.com/dc3pogjef/video/upload/v1693791074/Anuncio_para_Redes_Asia%CC%81tico_Restaurante_Entrega_a_Domicilio_6_fbzmhp.mp4"
-              type="video/mp4"
-            />
-          </video>
-        </SwiperSlide>
+    
+    <div className="w-full h-[65vh] min-h-[400px] max-h-[700px] overflow-hidden clip-slider-home">
+
+        <Swiper
+          centeredSlides={true}
+          effect={'fade'}
+          autoplay={{
+            delay: 7000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: false,
+          }}
+          navigation
+          modules={[EffectFade, Navigation, Pagination, Autoplay]}
+          className="h-full w-full"
+        >
         {/* Se pueden agregar más diapositivas de video según sea necesario */}
-      </Swiper>
+          <SwiperSlide>
+            <video autoPlay muted loop className="w-full h-full object-cover">
+              <source
+                src="https://res.cloudinary.com/dc3pogjef/video/upload/v1693791075/Anuncio_para_Redes_Asia%CC%81tico_Restaurante_Entrega_a_Domicilio_8_igxllh.mp4"
+                type="video/mp4"
+              />
+            </video>
+          </SwiperSlide>
+          <SwiperSlide>
+            <video autoPlay muted loop className="w-full h-full object-cover">
+              <source
+                src="https://res.cloudinary.com/dc3pogjef/video/upload/v1693791075/Anuncio_para_Redes_Asia%CC%81tico_Restaurante_Entrega_a_Domicilio_7_clwjri.mp4"
+                type="video/mp4"
+              />
+            </video>
+          </SwiperSlide>
+          <SwiperSlide>
+            <video autoPlay muted loop className="w-full h-full object-cover">
+              <source
+                src="https://res.cloudinary.com/dc3pogjef/video/upload/v1693791074/Anuncio_para_Redes_Asia%CC%81tico_Restaurante_Entrega_a_Domicilio_6_fbzmhp.mp4"
+                type="video/mp4"
+              />
+            </video>
+          </SwiperSlide>
+        </Swiper>
+
     </div>
+
   );
 }
 
